d69397c4a20874ed08c0f0266ada0eba256b5fdf
Ancelia Mathias Patrao <55922585+anceliaa06@users.noreply.github.com>
2025-06-17 22:33:44 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(num % 2 == 0 ? "Even number" : "Odd number");
+    }
+}
