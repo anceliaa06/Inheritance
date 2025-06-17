31cd6bf4ba2d3cc735565efb48244185efefb739
Ancelia Mathias Patrao <55922585+anceliaa06@users.noreply.github.com>
2025-06-17 22:03:50 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Animal
+{
+    public void Speak()
+    {
+        Console.WriteLine("Animal makes a sound.");
+    }
+}
+
+class Dog : Animal
+{
+    public void Bark()
+    {
+        Console.WriteLine("Dog barks.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Dog myDog = new Dog();
+        myDog.Speak();  // Inherited method
+        myDog.Bark();   // Dog-specific method
+    }
+}
