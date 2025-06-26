151edcc2ecead2cbd4c21fd6226f8b627a2f6af7
Ancelia Mathias Patrao <55922585+anceliaa06@users.noreply.github.com>
2025-06-26 22:21:11 +0530

    Update 2Program.cs

--- 2Program.cs
+++ 2Program.cs
@@ -1,1 +1,26 @@
+using System;
+
+class Student
+{
+    private string name;
+
+    public Student(string name)
+    {
+        this.name = name; // Referring to class-level variable
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Student Name: {this.name}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student student = new Student("Ancelia");
+        student.Display();
+    }
+}
 
